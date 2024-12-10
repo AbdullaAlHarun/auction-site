4b2aeb6cd3384b00068365fd007eb8c6f818f4dd
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-12-10 22:53:15 +0100

    Completed environment setup with Vite, Tailwind CSS, and DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './index.html',
+      './account/auth.html',
+      './profile/index.html',
+      './profile/create.html',
+      './listing/singleListing.html',
+      './src/**/*.{html,js}',
+  ],
+  theme: {
+      extend: {
+          fontFamily: {
+              parkinsans: ['Parkinsans', 'sans-serif'],
+              kantumruy: ['Kantumruy', 'sans-serif'],
+              montserrat: ['Montserrat', 'sans-serif'],
+          },
+      },
+  },
+  plugins: [
+      require('daisyui'),
+  ],
+  daisyui: {
+      themes: false,
+      darkTheme: "light",
+      base: true,
+      styled: true,
+      utils: true,
+      prefix: "",
+      themeRoot: ":root",
+  },
+};
